2422cd3eb457f6565695d66994780edb48018af0
Stanley Zheng <dev.stanzheng@gmail.com>
2016-11-17 21:14:06 -0500

    save file function

--- data/scrape.py
+++ data/scrape.py
@@ -9,6 +9,7 @@ BASE_URL = "http://primes.utm.edu/lists/small/millions/primes{}.zip"
 def unzip_file(file_url):
     """Downloads a file over the network and unzips it"""
     url = requests.get(file_url)
+    save_file(url)
     zipfile = ZipFile(BytesIO(url.content))
     zip_names = zipfile.namelist()
     # if unzipped return 1
@@ -17,7 +18,17 @@ def unzip_file(file_url):
         extracted_file = zipfile.open(file_name)
         return extracted_file
 
-def download_files(start=1, end=2):
+def save_file(req):
+    """Saves a file from file context"""
+    local_filename = req.url.split('/')[-1]
+    with open(local_filename, 'wb') as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+            return local_filename
+
+def download_files(start=1, end=3):
     """Iterate over the primes on the site"""
     for page_num in range(start, end):
         parse_zip_file(unzip_file(BASE_URL.format(page_num)))
